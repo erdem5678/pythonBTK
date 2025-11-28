1efafdb978c437065c34288fb502507b419d8acb
erdem5678 <bariserdemcaliskan@gmail.com>
2025-11-29 02:05:04 +0300

    update

--- python  programlama dili (temel)/bolum 8 dosya yönetimi/dosya-okuma.py
+++ python  programlama dili (temel)/bolum 8 dosya yönetimi/dosya-okuma.py
@@ -1,14 +1,19 @@
-""" 
-dosya açmak ve oluşturmak için open() fonksiyonu kullanilir.
+# f = open("open.txt","r")
+# print(f.read())
+for i in range(1,5):
+ with open("open.txt", "a") as f:
+    f.write(f"{i}. {input("kelime girin: ")}\n")
+# dosyayı okuma gösterme 
+with open("open.txt", encoding="utf-8") as f:
+    f = f.read()
+    print(f)
+aranan = input("aranan kişi kim: ")
+if aranan in f:
+   print(f"{aranan} kişi var ")
+else:
+   print(f"{aranan} kişi yok ")
 
 
-kulanimi              : open(dosya_adi,dosya_erişimi_modu)
-dosya_erişim_modu     : dosyayi hangi amaçla açtigimizi belirtilir.
-"r"                   : okuma modu. belirtilen konumda dosya olmalidir
-seek                  : cursor konumu 
-
-"""
-# 3 read den sonra bir şey yazdırmıyor çünkü cursor en sona gelmiş durumda.
-f = open("log.txt",encoding="utf-8")# varsayılan okuma modudur.
-print(f.read())
-print(f.read())
+#open and read the file after the appending:
+# with open("open.txt") as f:
+#   print(f.read()) 
